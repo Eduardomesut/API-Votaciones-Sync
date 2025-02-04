700f28acd17dc83cb166522242566b590148aee9
Eduardomesut <eduardomesut@gmail.com>
2025-02-04 13:10:59 +0100

    Iniciación Spring

---
+++ api-votaciones-sync/src/main/java/com/example/api_votaciones_sync/ApiVotacionesSyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_votaciones_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVotacionesSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVotacionesSyncApplication.class, args);
+	}
+
+}
---
+++ api-votaciones-sync/src/test/java/com/example/api_votaciones_sync/ApiVotacionesSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_votaciones_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVotacionesSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
